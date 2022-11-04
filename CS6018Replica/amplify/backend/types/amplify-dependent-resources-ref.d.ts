dcca216816f312bba420a3656b57d407f1cd1c4a
Terra Tidwell <terraktidwell@gmail.com>
2022-11-04 16:50:24 -0600

    amplify stuff

---
+++ CS6018Replica/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "lifestyleapp": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
